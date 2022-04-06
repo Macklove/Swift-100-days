6159660b3a5359b1967e66d7c576c75f39037779
Macklove <54309076+Macklove@users.noreply.github.com>
2022-04-06 20:21:59 +0300

    Project 7

---
+++ Project7/Project7/ViewController.swift
@@ -0,0 +1,104 @@
+import UIKit
+
+class ViewController: UITableViewController {
+    var petitions = [Petition]()
+    var tempPetitions = [Petition]()
+    var FiltredPetition = [Petition]()
+    
+    let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+                
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .compose, target: self,action: #selector(ShowSourceFromWhere))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(RightBarButtonForText))
+        
+        if let url = URL(string: urlString){
+            if let data = try? Data(contentsOf: url) {
+                parse(json: data)
+                return
+            }
+        }
+        ShowError()
+    }
+    
+    
+    @objc func RightBarButtonForText(){
+        
+        let ac = UIAlertController(title: "Введите текст для поиска", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        ac.addAction(UIAlertAction(title: "Назад", style: .default, handler: GetBackAllPetitions))
+        
+        let searchAction = UIAlertAction(title: "Поиск", style: .default) {
+            [weak self, weak ac] _ in
+            guard let searchingWord = ac?.textFields?[0].text else { return }
+            self?.FilteringPetitions(searchingWord)
+         
+        }
+        ac.addAction(searchAction)
+        present(ac, animated: true)
+    }
+    
+    func GetBackAllPetitions(_ action: UIAlertAction){
+        tempPetitions = petitions
+        tableView.reloadData()
+    }
+    
+        @objc func FilteringPetitions(_ word: String){
+        FiltredPetition.removeAll()
+        tempPetitions = petitions
+        var found = 0
+        var count = 0
+        for petition in tempPetitions {
+            if petition.body.contains(word){
+                FiltredPetition.append(tempPetitions[count].self)
+                found += 1
+            }
+            count += 1
+        }
+        if found >= 1 {
+            tempPetitions = FiltredPetition
+            tableView.reloadData()
+        }
+    }
+    
+    @objc func ShowSourceFromWhere () {
+        let ac = UIAlertController(title: "Источник", message: urlString, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "ОК", style: .cancel))
+        present(ac, animated: true)
+    }
+    
+    public func ShowError() {
+        let ac = UIAlertController(title: "Ошибка подключения", message: "Произошла ошибка при установлении соединения. Пожалуйста, проверьте подключение и повторите попытку.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "ОК", style: .default))
+        present(ac, animated: true)
+    }
+    
+    public func parse(json: Data) {
+        let decoder = JSONDecoder()
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json)
+        {
+            petitions = jsonPetitions.results
+            tempPetitions = jsonPetitions.results
+            tableView.reloadData()
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tempPetitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let petition = tempPetitions[indexPath.row]
+        cell.textLabel?.text = petition.title
+        cell.detailTextLabel?.text = petition.body
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailViewController()
+        vc.selectedPetition = tempPetitions[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
+    }
+}
